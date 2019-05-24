27433912d828171c31b1c1a54ac8e6159599c460
Khristian Punzalan <khrispunzalan@gmail.com>
2019-05-24 13:21:46 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-  if speed > 60 speed > 60  && speed < 40 
+  if speed > 60 || speed < 40 
     return "true"
   
   else
@@ -7,9 +7,9 @@ def unsafe?(speed)
 end
 
 def not_safe?(speed)
-	speed > 60  && speed < 40 ? true : false
+	speed > 60 || speed < 40 ? true : false
 end
 
-end
+
 
 
